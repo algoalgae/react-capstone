2a8b1fbd807b3a365fb7f4d7191a0c2e88322bf5
sagar <sagar.salimath@gmail.com>
2025-08-22 01:04:35 -0400

    adjust

--- little-lemon-capstone/src/component/booking/BookingConfirmation.js
+++ little-lemon-capstone/src/component/booking/BookingConfirmation.js
@@ -3,7 +3,7 @@ import '../../assets/css/layout.css';
 
 function BookingConfirmation() {
   return (
-    <div className="booking-page">
+    <div className="confirm-page">
       <div className="confirm-container">
       <h1 className="confirm-title">Booking Confirmed</h1>
       <p className="confirm-message">Your reservation has been successfully submitted.</p>
